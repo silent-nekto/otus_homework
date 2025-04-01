af59697ad142e87010064bf26ca5b7659612adad
Sergei Maslov <s.maslov@bi.zone>
2025-04-01 21:05:13 +0300

    Pre-commit config

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -4,11 +4,7 @@ import re
 import dataclasses
 
 
-config = {
-    "REPORT_SIZE": 1000,
-    "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
-}
+config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 @dataclasses.dataclass
@@ -21,11 +17,11 @@ class LastLog:
 
 def find_last_log(log_dir: str):
     last_log = None
-    pattern = re.compile(r'nginx-access-ui.log-(\d{8})(\.gz)?$')
+    pattern = re.compile(r"nginx-access-ui.log-(\d{8})(\.gz)?$")
     for name in os.listdir(log_dir):
         m = re.match(pattern, name)
         if m:
-            d = datetime.datetime.strptime(m.group(1), '%Y%m%d')
+            d = datetime.datetime.strptime(m.group(1), "%Y%m%d")
             if last_log is None or d > last_log.date:
                 last_log = LastLog(os.path.join(log_dir, name), name, d, m.group(2))
                 pass
